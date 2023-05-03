48de391846b49be7248be973ba785c09b1119f7b
AbuUsaid <usaidabu321@gmail.com>
2023-05-03 15:44:21 +0530

    Make width and height of gridbox dynamic

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const createGrid = (amtOfGrids) => {
     row.classList.add('grid-row');
 
     for (let j = 0; j < amtOfGrids; j++) {
+      const widthAndHeight = 560 / sizeOfGrid; //make width and height of boxes dynamic
       const gridBox = document.createElement('div');
       gridBox.classList.add('grid-box');
+      gridBox.style.width = `${widthAndHeight}px`;
+      gridBox.style.height = `${widthAndHeight}px`;
       row.appendChild(gridBox);
     }
 
